292a5bdae0c62712251b3a3988df2dd9eb6146da
anastasia-rez <nastya.reznichencko21@gmail.com>
2023-07-06 18:56:43 +0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,64 +1,3 @@
-// const urlCountry ='https://restcountries.com/v3.1/all';
-
-// const urlBank = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
-
-
-// let rates = await fetch(urlBank);
-//     rates = await rates.json();
-
-//     // rates = rates.results;
-// console.log(rates[24].r030);
-
-
-
-// let country = await fetch(urlCountry);
-//     country = await country.json();   
-    
-//     // country = country.results;
-
-// console.log(country[224].ccn3);
-
-
-
-// let infoClass = document.getElementById('info');
-
-// console.log(infoClass);
-
-
-
-// const totalInfo = rates.concat(country);
-
-// console.log(totalInfo);
-
-// for(let tag of totalInfo){
-
-//     if(rates[tag.r030] === country[tag.ccn3]){
-//         infoClass.innerHTML = totalInfo.map(item =>`
-//         <div class="card border rounded-4 border-3 border-secondary p-3 m-3">
-//             <p class="fs-4">Валюта: ${item.txt} (${item.cc})</p>
-//             <p class="fs-4">Курс: ${item.rate}</p>
-//             <p class="fs-4">Флаг:</p>
-//             <img style="width:150px" src="${item.flags.png}" alt="...">
-//         </div>
-//         `).join('');
-//     } else if(rates[tag.r030] != country[tag.ccn3]) {
-//         infoClass.innerHTML = '...';
-        
-//     }
-// }
-
-
-
-
-
-// currencies
-
-
-
-
-
-
-
 
 
 const urlCountry ='https://restcountries.com/v3.1/all';
@@ -69,39 +8,53 @@ const urlBank = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?js
 let rates = await fetch(urlBank);
     rates = await rates.json();
 
-    // rates = rates.results;
-console.log(rates[24].r030);
+console.log(rates);
 
 
 
-let country = await fetch(urlCountry);
-    country = await country.json();   
-    
-    // country = country.results;
+let countries = await fetch(urlCountry);
+    countries = await countries.json();   
 
-console.log(country[224].ccn3);
+console.log(countries);
+
+
+
+for(let rate of rates){
+    rate.flags = [];
+    rate.countryName = [];
+
+
+    for(let country of countries){
+        for(let code in country.currencies) {
+            if(code == rate.cc){
+                rate.flags.push(country.flags.png);
+                rate.countryName.push(country.name.common);
+            }
+        }
+    }
+}
+
+rates.pop();
+rates.pop();
+rates.pop();
+rates.pop();
+
+console.log(rates);
+
+
+
 
 
 
 let infoClass = document.getElementById('info');
 
-console.log(infoClass);
 
-const totalInfo = rates.concat(country);
-
-for(let tag of totalInfo){
-    
-    if(tag.r030 === tag.ccn3){
-        infoClass.innerHTML = totalInfo.map(item =>`
-        <div class="card border rounded-4 border-3 border-secondary p-3 m-3">
+infoClass.innerHTML = rates.map((item) =>`
+        <div class="border rounded-4 border-3 border-secondary p-3 m-3">
             <p class="fs-4">Валюта: ${item.txt} (${item.cc})</p>
             <p class="fs-4">Курс: ${item.rate}</p>
             <p class="fs-4">Флаг:</p>
-            <img style="width:150px" src="${item.flags}" alt="...">
+            
+            <p title="${item.countryName}"><img style="width:150px"  src="${item.flags}" alt="...">  </p>
         </div>
         `).join('');
-
-    }
-        
-
-}
